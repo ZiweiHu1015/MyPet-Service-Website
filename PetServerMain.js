8ba144b4183f9529035e882dfd58d538ad65bd7e
ZiweiHu1015 <36269090+ZiweiHu1015@users.noreply.github.com>
2020-04-24 20:07:31 -0400

    port

--- PetServerMain.js
+++ PetServerMain.js
@@ -1,5 +1,6 @@
 'use strict';
 exports.__esModule = true;
+var PORT = process.env.PORT || 8080;
 var mongo_database_1 = require("./mongo-database");
 var myserver_post_1 = require("./myserver-post");
 var theDatabase = new mongo_database_1.Database('perryguo98'); // CHANGE THIS
---
+++ Procfile
@@ -0,0 +1,2 @@
+
+web: node PetServerMain.js
