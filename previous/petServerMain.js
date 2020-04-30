4bfbcdde5179632848b5aec8588cb0ac34502ede
Perry <55702180+perryguo98@users.noreply.github.com>
2020-04-29 21:41:10 -0400

    updateandclean

--- previous/petServerMain.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-exports.__esModule = true;
-var mongo_database_1 = require("./mongo-database");
-var myserver_post_1 = require("./myserver-post");
-var theDatabase = new mongo_database_1.Database('perryguo98'); // CHANGE THIS
-var theServer = new myserver_post_1.MyServer(theDatabase);
-theServer.listen(process.env.PORT);
-
